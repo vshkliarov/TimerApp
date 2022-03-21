3ef92f6f577cc69649eb2590e17ca014277114f5
Vitalii Shkliarov <v.shklyarov@gmail.com>
2022-03-21 15:33:42 +0200

    program

---
+++ TimerApp.App/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TimerApp.App;
+
+class Program
+{
+    static void PrintTime(object state)
+    {
+        Console.WriteLine("Time is: {0}, Param is: {1}", DateTime.Now.ToLongTimeString(), state.ToString());
+    }
+    
+    static void Main(string[] args)
+    {
+        Console.WriteLine("\n***** Working with Timer type *****\n");
+        TimerCallback timeCB = new TimerCallback(PrintTime);
+
+        Timer t = new Timer(timeCB, "Param from Main()", 0, 1000);
+        Console.WriteLine("Hit key to terminate...");
+        Console.ReadLine();
+    }
+}
